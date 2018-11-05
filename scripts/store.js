72ea4e7e5f55dc08702cc5d49dfe6f09ee215de1
taylormerrill <taylormerrill92@gmail.com>
2018-11-05 09:25:00 -0700

    finsished part one IFFE

---
+++ scripts/store.js
@@ -0,0 +1,15 @@
+const store = (function() {
+    const items = 
+        [
+      { id: cuid(), name: 'apples', checked: false },
+      { id: cuid(), name: 'oranges', checked: false },
+      { id: cuid(), name: 'milk', checked: true },
+      { id: cuid(), name: 'bread', checked: false }
+    ];
+     let hideCheckedItems = false;
+     let searchTerm = '';
+    return {
+        items, hideCheckedItems, searchTerm
+    }
+}());
+
